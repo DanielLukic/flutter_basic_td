9664017d8be1f211299dead49230f6f3f910e3c9
Daniel Lukic <daniel.lukic@gmx.net>
2024-05-24 21:35:00 +0200

    Have thor destroy projectiles

--- lib/ligma_balls/adversaries/thor.dart
+++ lib/ligma_balls/adversaries/thor.dart
@@ -1,9 +1,13 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 
 import '../components/common.dart';
+import '../components/smoke.dart';
+import '../enemies/projectile.dart';
 
-class Thor extends SpriteComponent {
+class Thor extends SpriteComponent with CollisionCallbacks {
   //
   final TiledObject object;
 
@@ -13,6 +17,20 @@ class Thor extends SpriteComponent {
   Future<void> onLoad() async {
     sprite = await game.loadSprite('thor.png');
     position = Vector2(object.x + 8, object.y - 8);
-    priority = 500;
+    priority = 50;
+    add(CircleHitbox(collisionType: CollisionType.active));
+  }
+
+  @override
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is Projectile) {
+      other.active = false;
+      other.add(RemoveEffect(delay: 0.25));
+      smokeAround(other.position, Vector2.all(16));
+    }
   }
 }
