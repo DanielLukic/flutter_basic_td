23ae9010b7827c1eb1ffebff72736efe923f566a
Daniel Lukic <daniel.lukic@gmx.net>
2024-05-27 08:32:35 +0200

    Click on level to go to next/prev level when dev is true

--- lib/ligma_balls/components/scoreboard.dart
+++ lib/ligma_balls/components/scoreboard.dart
@@ -35,18 +35,41 @@ class Scoreboard extends PositionComponent {
       pos.x += 32;
     }
 
-    add(LevelInfo());
+    add(LevelInfo(Vector2(120, 56)));
     add(PlacementPoints());
   }
 }
 
-class LevelInfo extends Component {
+class LevelInfo extends PositionComponent with TapCallbacks {
+  LevelInfo(Vector2 position) {
+    this.position = position;
+  }
+
+  @override
+  bool containsLocalPoint(Vector2 point) {
+    final x = point.x;
+    final y = point.y;
+    if (x < 0 || y < 0 || x > 80 || y > 16) return false;
+    return true;
+  }
+
+  @override
+  void onTapUp(TapUpEvent event) {
+    super.onTapUp(event);
+    if (!dev) return;
+    if (event.localPosition.x > 40) {
+      world.nextLevel();
+    } else {
+      world.prevLevel();
+    }
+  }
+
   @override
   void render(Canvas canvas) {
     super.render(canvas);
     menuFont.scale = 0.5;
     menuFont.tint(textColor);
-    menuFont.drawText(canvas, 120, 56, 'Level ${level.id}');
+    menuFont.drawText(canvas, 0, 0, 'Level ${level.id}');
   }
 }
 
