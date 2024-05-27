6c533fcf5bd5e00c9180d1f059cf19585ceca10a
Daniel Lukic <daniel.lukic@gmx.net>
2024-05-27 12:10:27 +0200

    Add GameOverDialog component

---
+++ lib/ligma_balls/components/game_over_dialog.dart
@@ -0,0 +1,49 @@
+import 'package:flame/components.dart';
+
+import 'bitmap_button.dart';
+import 'bitmap_text.dart';
+import 'common.dart';
+import 'dialog.dart';
+import 'ligma_world.dart';
+
+class GameOverDialog extends Component {
+  @override
+  onLoad() async {
+    final dialog = Dialog(
+      await images.load('textbox.png'),
+      width: 240,
+      height: 100,
+    );
+    dialog.add(BitmapText(
+      text: 'Game Over',
+      position: Vector2(68, 12),
+      font: menuFont,
+      scale: 0.5,
+      tint: textColor,
+    ));
+    dialog.add(BitmapText(
+      text: 'Prime\'s Moustache(TM) has been destroyed!',
+      position: Vector2(16, 32),
+      font: textFont,
+      scale: 0.5,
+      tint: errorColor,
+    ));
+    dialog.add(BitmapButton(
+      bgNinePatch: await images.load('buttonbox.png'),
+      cornerSize: 8,
+      text: 'Retry Level',
+      position: Vector2(80, 60),
+      size: Vector2(80, 24),
+      fontScale: 0.5,
+      shortcuts: ['Enter', ' '],
+      onTap: () {
+        world.loadLevel();
+        parent?.remove(this);
+      },
+    ));
+    add(dialog);
+  }
+
+  @override
+  bool containsLocalPoint(Vector2 point) => true;
+}
